e3cb347823fd8935c5fb0ff7740a9f51e91985c8
aileapatrinely <aileapatrinely@gmail.com>
2023-03-25 17:24:21 -0500

    Removes datasource

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { UserEntity } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
-    private dataSource: DataSource,
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
   ) {}
