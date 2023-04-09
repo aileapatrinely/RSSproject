9c36b4fdb872c8199d334a6bae49d2538028f01b
aileapatrinely <aileapatrinely@gmail.com>
2023-04-09 11:59:47 -0500

    Adds svg plugin

--- rss-client/vite.config.js
+++ rss-client/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import url from '@rollup/plugin-url'
+import svg from '@rollup/plugin-svg'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,7 @@ export default defineConfig({
           '**/*.ico',
         ]
       }),
+      svg(),
     ],
     resolve: {
         alias: {
